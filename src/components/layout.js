366939fee3668634d44cf2b495a885f2d1445bb4
rezanotalive <hai.rezaprastya@gmail.com>
2021-03-04 11:02:02 +0700

    update styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react"
 
-import Header from './header'
-import Footer from './footer'
+import Header from "./header"
+import Footer from "./footer"
+import "../styles/index.scss"
 
-const Layout = (props) => {
-    return (
-        <div>
-            <Header />
-                {props.children}
-            <Footer />
-        </div>
-    )
+const Layout = props => {
+  return (
+    <div>
+      <Header />
+      {props.children}
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
