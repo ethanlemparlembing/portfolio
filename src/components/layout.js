997e2c6af9477c9c752472edbf60936d31965884
rezanotalive <hai.rezaprastya@gmail.com>
2021-03-05 10:31:56 +0700

    update style again

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,14 @@ import React from "react"
 import Header from "./header"
 import Footer from "./footer"
 import "../styles/index.scss"
+import "./layout.scss"
 
 const Layout = props => {
   return (
-    <div>
+    <div
+      className="container {
+      "
+    >
       <Header />
       {props.children}
       <Footer />
